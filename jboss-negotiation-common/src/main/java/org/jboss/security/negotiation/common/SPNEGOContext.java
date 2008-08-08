b52037d7673d000f240f372abbef20c8f56db951
Darran Lofthouse <darran.lofthouse@jboss.com>
2008-08-08 12:31:13 +0000

    [SECURITY-217] Project refactoring.

--- jboss-negotiation-common/src/main/java/org/jboss/security/negotiation/common/SPNEGOContext.java
+++ jboss-negotiation-common/src/main/java/org/jboss/security/negotiation/common/SPNEGOContext.java
@@ -20,7 +20,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.security.negotiation.spnego;
+package org.jboss.security.negotiation.common;
 
 import org.apache.log4j.Logger;
 import org.ietf.jgss.GSSContext;

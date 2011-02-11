91c416c09dbf995c23ef7e517dad82d09ec735e3
Darran Lofthouse <darran.lofthouse@jboss.com>
2011-02-11 14:58:48 +0000

    [SECURITY-568] Move networking code under a new project.

--- jboss-negotiation-net/src/main/java/org/jboss/security/negotiation/net/SPNEGOServerSocketFactory.java
+++ jboss-negotiation-net/src/main/java/org/jboss/security/negotiation/net/SPNEGOServerSocketFactory.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.security.negotiation.spnego.net;
+package org.jboss.security.negotiation.net;
 
 import java.io.IOException;
 import java.net.InetAddress;

0dc9d191b6eb1d13b8f0189c5b02ba6576f4722e
spolti <filippespolti@gmail.com>
2015-03-26 12:41:20 -0300

    SECURITY-877

--- jboss-negotiation-extras/src/main/java/org/jboss/security/negotiation/AdvancedLdapLoginModule.java
+++ jboss-negotiation-extras/src/main/java/org/jboss/security/negotiation/AdvancedLdapLoginModule.java
@@ -908,11 +908,14 @@ public class AdvancedLdapLoginModule extends CommonLoginModule
          tmp.putAll(env);
          String credentials = tmp.getProperty(Context.SECURITY_CREDENTIALS);
          String bindCredential = tmp.getProperty(BIND_CREDENTIAL);
-         if (credentials != null && credentials.length() > 0)
-            tmp.setProperty(Context.SECURITY_CREDENTIALS, "***");
          
-         if (bindCredential != null && bindCredential.length() > 0)
-             tmp.setProperty(BIND_CREDENTIAL, "***");
+         if (credentials != null && credentials.length() > 0) {
+        	 tmp.setProperty(Context.SECURITY_CREDENTIALS, "***");
+         }
+            
+         if (bindCredential != null && bindCredential.length() > 0) {
+        	 tmp.setProperty(BIND_CREDENTIAL, "***");
+         }
          
          log.trace("Logging into LDAP server, env=" + tmp.toString());
       }

6a0ae2ccd929e5ed8246ba39c1026d6f1dc2ed92
Darran Lofthouse <darran.lofthouse@jboss.com>
2015-09-22 15:49:25 +0100

    [SECURITY-910] Ensure long Base64 messages are not split with new lines as this breaks the format of the HTTP responses sent by Undertow.

--- jboss-negotiation-common/src/main/java/org/jboss/security/negotiation/NegotiationMessage.java
+++ jboss-negotiation-common/src/main/java/org/jboss/security/negotiation/NegotiationMessage.java
@@ -1,16 +1,16 @@
 /*
- * Copyright © 2008  Red Hat Middleware, LLC. or third-party contributors as indicated 
- * by the @author tags or express copyright attribution statements applied by the 
- * authors. All third-party contributions are distributed under license by Red Hat 
+ * Copyright © 2008  Red Hat Middleware, LLC. or third-party contributors as indicated
+ * by the @author tags or express copyright attribution statements applied by the
+ * authors. All third-party contributions are distributed under license by Red Hat
  * Middleware LLC.
  *
- * This copyrighted material is made available to anyone wishing to use, modify, copy, 
- * or redistribute it subject to the terms and conditions of the GNU Lesser General 
- * Public License, v. 2.1. This program is distributed in the hope that it will be 
- * useful, but WITHOUT A WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for 
- * more details. You should have received a copy of the GNU Lesser General Public License, 
- * v.2.1 along with this distribution; if not, write to the Free Software Foundation, Inc., 
+ * This copyrighted material is made available to anyone wishing to use, modify, copy,
+ * or redistribute it subject to the terms and conditions of the GNU Lesser General
+ * Public License, v. 2.1. This program is distributed in the hope that it will be
+ * useful, but WITHOUT A WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details. You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
@@ -24,7 +24,7 @@ import org.picketbox.commons.cipher.Base64;
 
 /**
  * The common base type for all negotiation messages.
- * 
+ *
  * @author darran.lofthouse@jboss.com
  * @since 10th August 2008
  * @version $Revision$
@@ -33,7 +33,7 @@ public abstract class NegotiationMessage
 {
 
    /**
-    * Write the message to the provided output stream in 
+    * Write the message to the provided output stream in
     * it's raw form.
     */
    public abstract void writeTo(final OutputStream os) throws IOException;
@@ -42,8 +42,8 @@ public abstract class NegotiationMessage
     * @return The message type.
     */
    public abstract String getMessageType();
-   
-   
+
+
    /**
     * Write the message to the provided output stream, if base64 is set the
     * output should be base64 encoded.
@@ -54,7 +54,7 @@ public abstract class NegotiationMessage
       {
          ByteArrayOutputStream baos = new ByteArrayOutputStream();
          writeTo(baos);
-         String encoded = Base64.encodeBytes(baos.toByteArray());
+         String encoded = Base64.encodeBytes(baos.toByteArray(), Base64.DONT_BREAK_LINES);
          os.write(encoded.getBytes());
       }
       else
